5eaa61d4678f0dea88706ef9732c34b7ce956210
The Hatter <quazariun@gmail.com>
2020-07-21 20:12:55 -0300

    Extract CSS to files for each page and update de styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,17 +15,4 @@
 //= require turbolinks
 //= require_tree .
 
-function ready(event) {
-  $('input[type=radio][name=searchRadio]').change(function() {
-    if (this.value === 'simple') {
-      $('.simple-form').show();
-      $('.advanced-form').hide();
-    }
-    else if (this.value === 'advanced') {
-      $('.simple-form').hide();
-      $('.advanced-form').show();
-    }
-  });
-}
-
 $(document).on('turbolinks:load', ready);
--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,25 +1,14 @@
 document.addEventListener("turbolinks:load", function() {
   $input = $("input[data-behavior='autocomplete']")
 
-  var options = {
-    getValue: "name",
-    url: function(phrase) {
-      return "/search?q=" + phrase;
-    },
-    categories: [
-      { 
-        listLocation: "power_generators"
-      }
-    ],
-    list: {
-      onChooseEvent: function() {
-        var url = $input.getSelectedItemData().url
-        $input.val("")
-        Turbolinks.visit(url)
-      }
-    }
+  url = function(phrase) {
+    return "/search?q=" + phrase;
   }
 
-  console.log(options);
+  document.addEventListener("onChooseEvent", function() {
+    var url = $input.getSelectedItemData().url
+    $input.val("")
+    Turbolinks.visit(url)
+  });
 
 });
